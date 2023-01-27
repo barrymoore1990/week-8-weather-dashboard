07be184fde2fc1cbe43db97173447dde10b8ded9
barrymoore1990 <hello@touringhighlights.com>
2023-01-27 17:57:52 +0000

    added ability to search from text input & click on previous search term

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,51 @@
-let city = "London"
-let queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=16c44a3c4ffd80699d455707a330e33e"
+let searchBtn = document.querySelector(".btn");
+let searchInput = document.querySelector("#search-input");
+let history = document.querySelector("#history");
+let listGroup = document.querySelector(".list-group");
 
-fetch(queryURL)
-.then(response => response.json())
-.then(citiesFound => {
-    var lat = citiesFound[0].lat;
-    var lon = citiesFound[0].lon;
-    console.log(lat, lon);
 
-    return fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=16c44a3c4ffd80699d455707a330e33e`)
-
+searchBtn.addEventListener("click", function(){
+    event.preventDefault()
+    let aaa = searchInput.value;
+    myFunction(aaa)
 })
 
-.then(response => response.json())
-// .then(function(response) {
-.then(data => {
-    console.log(data);
+
+
+
+
+listGroup.addEventListener("click", function(){
+    event.preventDefault()
+    let aaa = event.target.textContent
+    myFunction(aaa)
 })
+
+
+
+
+
+
+function myFunction(cityToSearchFor) {
+    let city = cityToSearchFor;
+    let createButton = document.createElement("BUTTON");
+    createButton.textContent = city;
+    createButton.className = "btn"
+    history.prepend(createButton);
+    let queryURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=16c44a3c4ffd80699d455707a330e33e"
+
+    fetch(queryURL)
+    .then(response => response.json())
+    .then(citiesFound => {
+        var lat = citiesFound[0].lat;
+        var lon = citiesFound[0].lon;
+        console.log(lat, lon);
+
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=16c44a3c4ffd80699d455707a330e33e`)
+
+    })
+
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+}

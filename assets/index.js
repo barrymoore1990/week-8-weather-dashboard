1a4669b99b7b8f941ee2cd564b3742cbe391af05
barrymoore1990 <hello@touringhighlights.com>
2023-01-28 05:59:36 +0000

    current day and 5 day forecast working

--- assets/index.js
+++ assets/index.js
@@ -2,6 +2,8 @@ let searchBtn = document.querySelector(".btn");
 let searchInput = document.querySelector("#search-input");
 let history = document.querySelector("#history");
 let listGroup = document.querySelector(".list-group");
+let currentDay = document.querySelector("#today");
+let forecastEl = document.querySelector("#forecast");
 
 
 searchBtn.addEventListener("click", function(){
@@ -22,7 +24,7 @@ listGroup.addEventListener("click", function(){
 })
 
 
-
+// Need to add in to local storage!!!!
 
 
 
@@ -32,7 +34,7 @@ function myFunction(cityToSearchFor) {
     createButton.textContent = city;
     createButton.className = "btn btn-secondary"
     history.prepend(createButton);
-    let queryURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=16c44a3c4ffd80699d455707a330e33e"
+    let queryURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&units=metric&appid=16c44a3c4ffd80699d455707a330e33e"
 
     fetch(queryURL)
     .then(response => response.json())
@@ -41,12 +43,53 @@ function myFunction(cityToSearchFor) {
         var lon = citiesFound[0].lon;
         console.log(lat, lon);
 
-        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=16c44a3c4ffd80699d455707a330e33e`)
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=16c44a3c4ffd80699d455707a330e33e`)
 
     })
 
     .then(response => response.json())
     .then(data => {
+        let date = moment(data.list[0].dt_txt).format('DD-MM-YYYY');;
+        let weatherIcon = "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png";
+        let temp = data.list[0].main.temp;
+        let wind = data.list[0].wind.speed;
+        let humidity = data.list[0].main.humidity;
         console.log(data);
+        console.log(data.list[8].weather[0].icon);
+        
+        currentDay.innerHTML = 
+        `<div class="current">
+        <h1>${data.city.name} (${date}) <img src="${weatherIcon}"></h1>
+        <p>Temp: ${temp} °C</p>
+        <p>Wind: ${wind} KPH</p>
+        <p>Humidity: ${humidity}%</p>
+        </div>`;
+
+        // Same as above but for loop for each day from now
+        // 8, 16, 24, 32, 39
+        function function2(p) {
+            date = moment(data.list[p].dt_txt).format('DD-MM-YYYY');;
+            weatherIcon = "http://openweathermap.org/img/wn/" + data.list[p].weather[0].icon + "@2x.png";
+            temp = data.list[p].main.temp;
+            wind = data.list[p].wind.speed;
+            humidity = data.list[p].main.humidity;
+
+            forecastEl.innerHTML += 
+            `<div class="fiveDay">
+            <h3>${date}</h3>
+            <img src="${weatherIcon}">
+            <p>Temp: ${temp} °C</p>
+            <p>Wind: ${wind} KPH</p>
+            <p>Humidity: ${humidity}%</p>
+            </div>`;
+        }
+        function2(8);
+        function2(16);
+        function2(24);
+        function2(32);
+        function2(39);
+
+        
     })
 }
+
